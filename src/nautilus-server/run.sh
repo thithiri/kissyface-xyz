386f7fb2b6d74cb24ff4a67def632bfd4974f760
thithiri <thiri.jess@gmail.com>
2025-11-25 21:24:10 +0700

    run.sh configured for allowed endpoints

--- src/nautilus-server/run.sh
+++ src/nautilus-server/run.sh
@@ -22,6 +22,14 @@ busybox ip link set dev lo up
 
 # Add a hosts record, pointing target site calls to local loopback
 echo "127.0.0.1   localhost" > /etc/hosts
+echo "127.0.0.64   api.weatherapi.com" >> /etc/hosts
+echo "127.0.0.65   api.together.xyz" >> /etc/hosts
+echo "127.0.0.66   huggingface.co" >> /etc/hosts
+echo "127.0.0.67   walrus-mainnet-publisher.nami.cloud" >> /etc/hosts
+echo "127.0.0.68   aggregator.walrus-mainnet.walrus.space" >> /etc/hosts
+echo "127.0.0.69   kissyface.xyz" >> /etc/hosts
+echo "127.0.0.70   www.kissyface.xyz" >> /etc/hosts
+
 
 
 # == ATTENTION: code should be generated here that parses allowed_endpoints.yaml and populate domains here ===
@@ -41,6 +49,14 @@ echo "$JSON_RESPONSE" | jq -r 'to_entries[] | "\(.key)=\(.value)"' > /tmp/kvpair
 
 # == ATTENTION: code should be generated here that added all hosts to forward traffic ===
 # Traffic-forwarder-block
+python3 /traffic_forwarder.py 127.0.0.64 443 3 8101 &
+python3 /traffic_forwarder.py 127.0.0.65 443 3 8102 &
+python3 /traffic_forwarder.py 127.0.0.66 443 3 8103 &
+python3 /traffic_forwarder.py 127.0.0.67 443 3 8104 &
+python3 /traffic_forwarder.py 127.0.0.68 443 3 8105 &
+python3 /traffic_forwarder.py 127.0.0.69 443 3 8106 &
+python3 /traffic_forwarder.py 127.0.0.70 443 3 8107 &
+
 
 # Listens on Local VSOCK Port 3000 and forwards to localhost 3000
 socat VSOCK-LISTEN:3000,reuseaddr,fork TCP:localhost:3000 &
